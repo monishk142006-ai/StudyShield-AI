a6a79c7206f28cb92aa4f2cbbaf4e342dedb358a
Monish K <monish.k142006@gmail.com>
2026-07-12 14:45:39 +0530

    Updated MySQL configuration for deployment

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ app.permanent_session_lifetime = timedelta(days=30)
 app.secret_key = "studyshield_secret_key"
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'project1'
-app.config['MYSQL_DB'] = 'studyshield'
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
 
 mysql = MySQL(app)
 
