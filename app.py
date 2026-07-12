687633ba0b7573f394854a8a6bba9e28bad39ef2
Monish K <monish.k142006@gmail.com>
2026-07-12 18:36:09 +0530

    Fix Railway MySQL environment variables

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ app.permanent_session_lifetime = timedelta(days=30)
 app.secret_key = "studyshield_secret_key"
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
-app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
+app.config['MYSQL_HOST'] = os.getenv("MYSQLHOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQLUSER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQLPASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQLDATABASE")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQLPORT"))
 
 mysql = MySQL(app)
 
