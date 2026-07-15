18dcd394e286279e0632a9b58bc6b1af41f99393
Monish K <monish.k142006@gmail.com>
2026-07-15 20:56:45 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -405,5 +405,11 @@ Study Material:
 
 
 # ---------------- RUN APP ----------------
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
